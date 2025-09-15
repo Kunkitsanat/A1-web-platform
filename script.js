9cf48ceffb3c6faca3dc6d975f72f8865363b24b
Kunkitsanat <kitsanatpiri@gmail.com>
2025-09-15 21:53:17 +0700

    create start_ball function

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let anime;
 
 function setup() {
     createCanvas(500, 500);
-    
+    start_ball();
 }
 
 function draw() {
     background(135, 206, 235);
     draw_player(mouseX);
+    ellipse(x,y,20,20);
+    y += count;
 }
 
 function draw_player(playerx) { 
@@ -20,3 +22,10 @@ function draw_player(playerx) {
     
 } 
 
+function start_ball() { 
+    x = int(random(50, 450));  
+    y = 0; 
+    count = int(random(5, 12)); 
+    ball_type = int(random(1, 6));
+    anime = (ball_type == 1);
+}
