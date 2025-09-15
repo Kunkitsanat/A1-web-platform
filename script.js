f74c6b7a800f41247a58bcf1129f3645f560c5bb
Kunkitsanat <kitsanatpiri@gmail.com>
2025-09-15 21:42:47 +0700

    create draw_player function

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ let count;
 let anime;
 
 function setup() {
-  createCanvas(500, 500);
+    createCanvas(500, 500);
     
 }
 
 function draw() {
     background(135, 206, 235);
-    
+    draw_player(mouseX);
 }
+
+function draw_player(playerx) { 
+    line(playerx - 20, 450, playerx + 20, 450);
+    line(playerx - 20, 450 - 30, playerx - 20, 450); 
+    line(playerx + 20, 450 - 30, playerx + 20, 450); 
+    
+} 
+
